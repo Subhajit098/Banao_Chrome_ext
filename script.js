1c0f61a5f49616ac56242047fcb3bb83f4a5ffda
androidjit07 <sd.20u10020@btech.nitdgp.ac.in>
2023-08-20 15:07:48 +0530

    updated script

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 //     var title = tab.title;
 //     document.getElementById("title").textContent=title;
 // })
-const  getCurrentTab=async()=>{
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    let tab = await chrome.tabs.getCurrent(title);
-    return tab.title;
-  }
+    // let queryOptions = { active: true, lastFocusedWindow: true };
+    // // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    // let tab = await chrome.tabs.getCurrent(title);
+    // return tab.title;
+    let new_title
 
-  const new_title=getCurrentTab();
+    chrome.tabs.getSelected(null,tab=>{
+       new_title =tab.title;
+    })
 
-  document.getElementById("title").innerHTML=new_title;
+  document.getElementById("title").textContent=new_title;
